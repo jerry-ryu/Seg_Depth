4224aaf3d1ffac1736c64321fb5a9a88a40f7812
jerry-ryu <fbrjs15@naver.com>
2023-11-18 15:26:03 +0900

    [feat] EDA code
    create infer_compare.py to EDA dataset with viz.
    Referenced Issue: #1

---
+++ infer_compare.py
@@ -0,0 +1,355 @@
+import argparse
+import os
+import sys
+import copy
+
+import numpy as np
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+import model_io
+from models import UnetAdaptiveBins
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from dataloader import DepthDataLoader
+from utils import RunningAverageDict
+import pandas as pd
+
+
+def compute_errors(gt, pred):
+    thresh = np.maximum((gt / pred), (pred / gt))
+    a1 = (thresh < 1.25).mean()
+    a2 = (thresh < 1.25 ** 2).mean()
+    a3 = (thresh < 1.25 ** 3).mean()
+
+    abs_rel = np.mean(np.abs(gt - pred) / gt)
+    sq_rel = np.mean(((gt - pred) ** 2) / gt)
+
+    rmse = (gt - pred) ** 2
+    rmse = np.sqrt(rmse.mean())
+
+    rmse_log = (np.log(gt) - np.log(pred)) ** 2
+    rmse_log = np.sqrt(rmse_log.mean())
+
+    err = np.log(pred) - np.log(gt)
+    silog = np.sqrt(np.mean(err ** 2) - np.mean(err) ** 2) * 100
+
+    log_10 = (np.abs(np.log10(gt) - np.log10(pred))).mean()
+    return dict(a1=a1, a2=a2, a3=a3, abs_rel=abs_rel, rmse=rmse, log_10=log_10, rmse_log=rmse_log,
+                silog=silog, sq_rel=sq_rel)
+
+def viz(impath, img, final, gt, validmask, bin_np, metric):
+    
+    if args.save_dir is not None:
+        if args.dataset == 'nyu':
+            if impath[0] == '/':
+                impath = impath[1:]
+            factor = 1000.
+        else:
+            dpath = impath.split('/')
+            impath = dpath[1] + "_" + dpath[-1]
+            impath = impath.split('.')[0]
+            factor = 256.
+        
+        pred_path = os.path.join(args.save_dir, impath)
+        pred = (final * factor).astype('uint16')
+
+        img = img.squeeze()
+        img = img.transpose((1,2,0))
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        denormalized_image = img * std + mean
+        img = denormalized_image.clip(0, 255)
+        #pred_map
+        pred_cmap = plt.get_cmap('plasma')
+        pred_norm = Normalize(vmin=0, vmax=5000)
+        
+        
+        #gt_map
+        valid_map = np.ones_like(gt)
+        valid_map[validmask==False] = np.NaN
+        
+        gt_map = (gt*factor)*valid_map
+        gt_cmap = plt.get_cmap('plasma')
+        gt_cmap.set_bad(color="Black")
+        gt_norm = Normalize(vmin=0, vmax=5000)
+        
+        # error map
+        error_cmap = plt.get_cmap("RdBu")
+        error_cmap.set_bad(color="Black")
+        error_norm = Normalize(vmin=-2000, vmax=2000)
+        error_map = (pred - gt*factor)*valid_map
+        
+        # bins
+        flt = gt[validmask].flatten()
+        bin_hist = bin_np[0].tolist()
+        
+        #chamfer_distance
+        from pytorch3d.loss import chamfer_distance
+        from torch.nn.utils.rnn import pad_sequence
+        bin_centers = 0.5 * (bin_np[:, 1:] + bin_np[:, :-1])
+        input_points = np.expand_dims(bin_centers,2)  # .shape = n, p, 1
+        input_points = torch.from_numpy(input_points)
+        # n, c, h, w = target_depth_maps.shape
+        
+        target_depth_maps = torch.from_numpy(gt)
+        target_depth_maps = target_depth_maps.unsqueeze(0)
+        target_points = target_depth_maps.flatten(1)  # n, hwc
+        mask = target_points.ge(1e-3)  # only valid ground truth points
+        target_points = [p[m] for p, m in zip(target_points, mask)]
+        target_lengths = torch.Tensor([len(t) for t in target_points]).long()
+        target_points = pad_sequence(target_points, batch_first=True).unsqueeze(2)  # .shape = n, T, 1
+
+        cham_dist, _ = chamfer_distance(x=input_points, y=target_points, y_lengths=target_lengths)
+        
+        # metric
+        metric_dict = copy.deepcopy(metric)
+        metric_dict["cham_dist"] = cham_dist.item()
+        metric_dict["path"] = pred_path
+        
+        # plot
+        fig = plt.figure(figsize= (15,10))
+        
+        # RGB
+        plt.subplot(3, 3, 1)
+        plt.imshow((img * 255).astype(np.uint8))
+        plt.title("RGB image")
+
+        # pred
+        plt.subplot(3, 3, 2)
+        plt.imshow(pred, cmap=pred_cmap, norm=pred_norm)
+        plt.colorbar()
+        plt.title("Pred depth")
+
+        # GT
+        plt.subplot(3, 3, 3)
+        plt.imshow(gt_map, cmap=gt_cmap, norm=gt_norm)
+        plt.colorbar()
+        plt.title("Ground Truth")
+
+        # Error_map
+        plt.subplot(3, 3, 4)
+        plt.imshow(error_map, cmap=error_cmap, norm=error_norm)
+        plt.colorbar()
+        plt.title("Error map (pred - gt)")
+        plt.text
+
+        plt.subplot(3, 3, 5)
+        plt.text(0.1,0.1,
+                 f"a1: {metric_dict['a1']}\na2: {metric_dict['a2']}\na3: {metric_dict['a3']}\
+                 \nrmse_log: {metric_dict['rmse_log']}\nrmse: {metric_dict['rmse']}", fontsize=16)
+        plt.axis("off")
+        plt.subplot(3, 3, 6)
+        plt.text(0.1,0.1,
+                 f"log_10: {metric_dict['log_10']}\nabs_rel: {metric_dict['abs_rel']}\nsilog: {metric_dict['silog']}\
+                 \nsq_rel: {metric_dict['sq_rel']}\ncham_dist: {metric_dict['cham_dist']}", fontsize=16)
+        plt.axis("off")
+        # pred_bins hist
+        plt.subplot(3, 3, 7)
+        n, bins_stand, patches = plt.hist(flt, bins = bin_hist, rwidth = 0.9)
+        cm = plt.get_cmap("viridis")
+        min_value = np.min(bins_stand)
+        max_value = np.max(bins_stand)
+        min_max_stand = max_value - min_value
+        bins_nom = [x / min_max_stand for x in bins_stand]
+        for c, p in zip(bins_nom, patches):
+            plt.setp(p, 'facecolor', cm(c))
+        plt.title("Depth hist. with Adabins")
+
+        # uniform_bins hist
+        plt.subplot(3, 3, 8)
+        n, bins, patches = plt.hist(flt, bins = 64)
+        bins_nom = [x / min_max_stand for x in bins]
+        for c, p in zip(bins_nom, patches):
+            plt.setp(p, 'facecolor', cm(c))
+            plt.xlim(min_value, max_value)
+        plt.title("Depth hist. with uniform bins")
+        
+        # bins hist
+        plt.subplot(3, 3, 9)
+        n, bins, patches = plt.hist(bin_hist, bins=20)
+        bins_nom = [x / min_max_stand for x in bins]
+        for c, p in zip(bins_nom, patches):
+            plt.setp(p, 'facecolor', cm(c))
+        plt.title("Hist. of  Adabins")
+        
+        # save
+        if not os.path.exists(os.path.dirname(pred_path)):
+            os.makedirs(os.path.dirname(pred_path))
+        plt.savefig(pred_path)
+        plt.close()
+        
+    return metric_dict
+        
+        
+
+# def denormalize(x, device='cpu'):
+#     mean = torch.Tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1).to(device)
+#     std = torch.Tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1).to(device)
+#     return x * std + mean
+#
+def predict_tta(model, image, args):
+    bins, pred = model(image)
+    #     pred = utils.depth_norm(pred)
+    #     pred = nn.functional.interpolate(pred, depth.shape[-2:], mode='bilinear', align_corners=True)
+    #     pred = np.clip(pred.cpu().numpy(), 10, 1000)/100.
+    pred = np.clip(pred.cpu().numpy(), args.min_depth, args.max_depth)
+
+    image = torch.Tensor(np.array(image.cpu().numpy())[..., ::-1].copy()).to(device)
+
+    pred_lr = model(image)[-1]
+    #     pred_lr = utils.depth_norm(pred_lr)
+    #     pred_lr = nn.functional.interpolate(pred_lr, depth.shape[-2:], mode='bilinear', align_corners=True)
+    #     pred_lr = np.clip(pred_lr.cpu().numpy()[...,::-1], 10, 1000)/100.
+    pred_lr = np.clip(pred_lr.cpu().numpy()[..., ::-1], args.min_depth, args.max_depth)
+    final = 0.5 * (pred + pred_lr)
+    final = nn.functional.interpolate(torch.Tensor(final), image.shape[-2:], mode='bilinear', align_corners=True)
+    return torch.Tensor(final), bins
+
+
+def eval(model, test_loader, args, gpus=None, ):
+    if gpus is None:
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    else:
+        device = gpus[0]
+
+    if args.save_dir is not None and not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+        
+    df = pd.DataFrame()
+    
+    metrics = RunningAverageDict()
+    # crop_size = (471 - 45, 601 - 41)
+    # bins = utils.get_bins(100)
+    total_invalid = 0
+    with torch.no_grad():
+        model.eval()
+
+        sequential = test_loader
+        for batch in tqdm(sequential):
+
+            image = batch['image'].to(device)
+            gt = batch['depth'].to(device)
+            final, bins = predict_tta(model, image, args)
+            final = final.squeeze().cpu().numpy()
+
+            # final[final < args.min_depth] = args.min_depth
+            # final[final > args.max_depth] = args.max_depth
+            final[np.isinf(final)] = args.max_depth
+            final[np.isnan(final)] = args.min_depth
+
+            if 'has_valid_depth' in batch:
+                if not batch['has_valid_depth']:
+                    # print("Invalid ground truth")
+                    total_invalid += 1
+                    continue
+
+            gt = gt.squeeze().cpu().numpy()
+            valid_mask = np.logical_and(gt > args.min_depth, gt < args.max_depth)
+
+            if args.garg_crop or args.eigen_crop:
+                gt_height, gt_width = gt.shape
+                eval_mask = np.zeros(valid_mask.shape)
+
+                if args.garg_crop:
+                    eval_mask[int(0.40810811 * gt_height):int(0.99189189 * gt_height),
+                    int(0.03594771 * gt_width):int(0.96405229 * gt_width)] = 1
+
+                elif args.eigen_crop:
+                    if args.dataset == 'kitti':
+                        eval_mask[int(0.3324324 * gt_height):int(0.91351351 * gt_height),
+                        int(0.0359477 * gt_width):int(0.96405229 * gt_width)] = 1
+                    else:
+                        eval_mask[45:471, 41:601] = 1
+            valid_mask = np.logical_and(valid_mask, eval_mask)
+            #             gt = gt[valid_mask]
+            #             final = final[valid_mask]
+            metric = compute_errors(gt[valid_mask], final[valid_mask])
+            metrics.update(metric)
+            
+            metric_dict = viz(batch["image_path"][0], image.cpu().numpy(), final, gt, valid_mask, bins.cpu().numpy(), metric)
+            df = pd.concat([df, pd.DataFrame.from_dict([metric_dict])])
+            
+    print(f"Total invalid: {total_invalid}")
+    metrics = {k: round(v, 3) for k, v in metrics.get_value().items()}
+    print(f"Metrics: {metrics}")
+    
+    df.to_csv(os.path.join(args.save_dir, "metric.csv"), index=False)
+
+
+def convert_arg_line_to_args(arg_line):
+    for arg in arg_line.split():
+        if not arg.strip():
+            continue
+        yield str(arg)
+
+
+if __name__ == '__main__':
+
+    # Arguments
+    parser = argparse.ArgumentParser(description='Model evaluator', fromfile_prefix_chars='@',
+                                     conflict_handler='resolve')
+    parser.convert_arg_line_to_args = convert_arg_line_to_args
+    parser.add_argument('--n-bins', '--n_bins', default=256, type=int,
+                        help='number of bins/buckets to divide depth range into')
+    parser.add_argument('--gpu', default=None, type=int, help='Which gpu to use')
+    parser.add_argument('--save-dir', '--save_dir', default=None, type=str, help='Store predictions in folder')
+    parser.add_argument("--root", default=".", type=str,
+                        help="Root folder to save data in")
+
+    parser.add_argument("--dataset", default='nyu', type=str, help="Dataset to train on")
+
+    parser.add_argument("--data_path", default='../dataset/nyu/sync/', type=str,
+                        help="path to dataset")
+    parser.add_argument("--gt_path", default='../dataset/nyu/sync/', type=str,
+                        help="path to dataset gt")
+
+    parser.add_argument('--filenames_file',
+                        default="./train_test_inputs/nyudepthv2_train_files_with_gt.txt",
+                        type=str, help='path to the filenames text file')
+
+    parser.add_argument('--input_height', type=int, help='input height', default=416)
+    parser.add_argument('--input_width', type=int, help='input width', default=544)
+    parser.add_argument('--max_depth', type=float, help='maximum depth in estimation', default=10)
+    parser.add_argument('--min_depth', type=float, help='minimum depth in estimation', default=1e-3)
+
+    parser.add_argument('--do_kb_crop', help='if set, crop input images as kitti benchmark images', action='store_true')
+
+    parser.add_argument('--data_path_eval',
+                        default="../dataset/nyu/official_splits/test/",
+                        type=str, help='path to the data for online evaluation')
+    parser.add_argument('--gt_path_eval', default="../dataset/nyu/official_splits/test/",
+                        type=str, help='path to the groundtruth data for online evaluation')
+    parser.add_argument('--filenames_file_eval',
+                        default="./train_test_inputs/nyudepthv2_test_files_with_gt.txt",
+                        type=str, help='path to the filenames text file for online evaluation')
+    parser.add_argument('--checkpoint_path', '--checkpoint-path', type=str, required=True,
+                       help="checkpoint file to use for prediction")
+
+    parser.add_argument('--min_depth_eval', type=float, help='minimum depth for evaluation', default=1e-3)
+    parser.add_argument('--max_depth_eval', type=float, help='maximum depth for evaluation', default=10)
+    parser.add_argument('--eigen_crop', help='if set, crops according to Eigen NIPS14', action='store_true')
+    parser.add_argument('--garg_crop', help='if set, crops according to Garg  ECCV16', action='store_true')
+    parser.add_argument('--do_kb_crop', help='Use kitti benchmark cropping', action='store_true')
+
+    if sys.argv.__len__() == 2:
+        arg_filename_with_prefix = '@' + sys.argv[1]
+        args = parser.parse_args([arg_filename_with_prefix])
+    else:
+        args = parser.parse_args()
+
+    import pprint
+    pprint.pprint(args)
+    
+    # args = parser.parse_args()
+    args.gpu = int(args.gpu) if args.gpu is not None else 0
+    args.distributed = False
+    device = torch.device('cuda:{}'.format(args.gpu))
+    test = DepthDataLoader(args, 'online_eval').data
+    model = UnetAdaptiveBins.build(n_bins=args.n_bins, min_val=args.min_depth, max_val=args.max_depth,
+                                   norm='linear').to(device)
+    model = model_io.load_checkpoint(args.checkpoint_path, model)[0]
+    model = model.eval()
+
+    eval(model, test, args, gpus=[device])
